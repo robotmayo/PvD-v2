94ed5350537adc536f08ae22b7a6428fe22a119c
robotmayo <robotmayo@gmail.com>
2013-06-28 19:57:34 -0400

    Contains data used by the game.
    Mostly enemy info, might throw in some configuration stuff in there.

---
+++ js/data.js
@@ -0,0 +1,11 @@
+var enemy_data = {
+	goblinKing : {
+		name : "Goblin King",
+		desc : "Despite what the name goblin implies, the Goblin King is a fearsome, hulking giant.",
+		hp : 4000,
+		mp : 1000,
+		satb : 50,
+		matb : 100,
+		atbc : 35
+	}
+}
