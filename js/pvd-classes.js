5095953aa3f69bedfc94d1072bef6072bc7e7743
robotmayo <robotmayo@gmail.com>
2013-06-28 20:02:14 -0400

    Using an ATB system over turnbased.
    Added some canvas rendering for the enemy.
    Must remember to update player to use atb. Not sure why the player
    wasn't the first one to receive this change.

--- js/pvd-classes.js
+++ js/pvd-classes.js
@@ -13,6 +13,9 @@ Character.prototype.critMul = 0;
 Character.prototype.alive = true;
 Character.prototype.name = "Character";
 Character.prototype.desc = "Base Class";
+Character.prototype.maxAtb = 100;
+Character.prototype.atb = 0;
+Character.prototype.atbRecRate = 25; 
 
 Character.prototype.updateHealth = function(value){
   this.health += value;
@@ -46,18 +49,66 @@ Character.prototype.calcMagicDamage = function(){
   return 1;
 };
 
+Character.prototype.update = function(){
+  this.updateAtb();
+};
+
+Character.prototype.updateAtb = function(){
+  this.atb += this.atbRecRate * elapsed;
+  if(this.atb > this.maxAtb){
+    this.atb = this.maxAtb;
+  }
+};
+// Expects a canvas context
+Character.prototype.render = function(context){
+  // Overide and render
+};
+Character.prototype.canvas = false;
+Character.prototype.canvasContext = false;
+Character.prototype.setCanvas = function(canvas){
+  this.canvas = canvas;
+  this.canvasContext = canvas.getContext("2d");
+}
+Character.prototype.getBar = function(min,max,width){
+  return Math.floor((min/max)*width);
+}
+
 function Hero(name){
   this.name = name;
 }
-Hero.prototype = Character.prototype;
+Hero.prototype = new Character();
 Hero.prototype.calcDamage = function(){
   return 999999;
 };
 var test = new Hero("Dick McPhearson");
-console.log(test.calcDamage());
 
-function Enemy(name,desc){
-  this.name = name;
-  this.desc = desc;
+/**
+Name {String} Enemy name
+Desc {String} Enemy description
+Hp {Int} Max Health
+MP {Int} Max Mana
+satb {Int} Starting ATB charge
+matb {Int} Maximum ATB charge
+atbc {Int} ATB Charge rate in x per seconds
+*/
+function Enemy(obj,name,desc,hp,mp,satb,matb,atbc){
+  this.name = obj.name || name;
+  this.desc = obj.desc || desc;
+  this.health = this.maxHealth = obj.hp || hp;
+  this.mana = this.maxMana = obj.mp || mp;
+  this.atb = obj.satb || satb;
+  this.maxAtb = obj.matb || matb;
+  this.atbRecRate = obj.atbc || atbc;
+  
+  this.render = function(){
+    this.canvasContext.clearRect(0,0,this.canvas.width,this.canvas.height);
+    this.canvasContext.fillStyle = "rgb(119,144,255)";
+    this.canvasContext.fillRect(0,75,this.getBar(this.atb,this.maxAtb,200),25);
+    this.canvasContext.fillStyle = "#AA0042";
+    this.canvasContext.fillRect(0,0,this.getBar(this.health,this.maxHealth,200),25);
+    this.canvasContext.fillStyle = "#4200AA";
+    this.canvasContext.fillRect(0,0,this.getBar(this.mana,this.maxMana,200),25);
+  };
+
 }
-Enemy.prototype = Character.prototype;
+Enemy.prototype = new Character();
