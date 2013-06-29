dbfa83d5691626531efa67b68b048b6ded3e7cd6
robotmayo <robotmayo@gmail.com>
2013-06-28 20:03:41 -0400

    Translated to JQuery. Button now works.

--- js/page-functions.js
+++ js/page-functions.js
@@ -1,73 +1,59 @@
+
 var charSelect;
 var nameInput;
 var goBtn;
 var start;
+var inputIntervalId;
+var nameValid;
 function updateCharState(){
 
 }
 
 // Sets up variables and event listeners
 function init(){
-    charSelect = document.getElementsByClassName("char-select");
-    nameInput = document.getElementById("name-input");
-    goBtn = document.getElementById("go");
-    start = document.getElementById("start");
-    if(nameInput.addEventListener){
-            nameInput.addEventListener("keyup",checkKey,false);
-        }else if(nameInput.attachEvent){
-            nameInput.attachEvent("keyup",checkKey,false);
-    }
-
-    for(var i = 0; i < charSelect.length; i++){
-        if(charSelect[i].addEventListener){
-            console.log(charSelect[i]);
-            charSelect[i].addEventListener("click",select,false);
-        }else if(charSelect[i].attachEvent){
-            charSelect[i].attachEvent("click",select,false);
-        }
-    }
-    setInterval(validateInput,100);
+    charSelect = $(".char-select");
+    nameInput = $("#name-input");
+    goBtn = $("#go");
+    start = $("#start");
+    nameInput.on("keyup",checkKey);
+    charSelect.on("click",function(){
+        $(this).closest("#character-selection")
+        .find(".char-select").removeClass("selected");
+        $(this).addClass("selected");
+    });
+    goBtn.on("click",function(){
+        if(nameValid){clearAndBegin();}
+    });
+    nameValid = false;
+    inputIntervalId = setInterval(validateInput,100);
 }
 function checkKey(evt){
-    if(evt.keyCode == 13){
-        if(document.readyState === "complete"){
-            start.classList.add("hide");
-            setInterval(startGame()); // Don't want it to block
-        }
+    if(evt.keyCode == 13 && nameValid == true){
+        clearAndBegin();
     }
 }
-function select(evt){
-    // Old versions of IE use srcElement....
-    var caller = event.target || event.srcElement;
-    // For some reason the select event is being called by the children
-    // Of the div instead of only the div itself.
-    if(caller.tagName != "DIV") caller = caller.parentNode;
-    if(caller.classList.contains("selected")) {return;}
-    for(var i = 0; i < charSelect.length; i++){
-        if(charSelect[i].classList.contains("selected")){
-            charSelect[i].classList.remove("selected")
-        }
-    }
-    caller.classList.add("selected");
-    console.log(caller);
+function clearAndBegin(){
+    start.addClass("hide");
+    clearInterval(inputIntervalId);
+    startGame(nameInput.val());
 }
 function validateInput(){
-    if(nameInput.value.length < 5){
-        if( !(nameInput.className == "error") ){
-            nameInput.className = "error";
+    if(nameInput.val().length < 5){
+        if( nameInput.hasClass("error") == false ){
+            nameInput.addClass("error");
         }
-        if( !(goBtn.className == "hidden") ){
-            goBtn.className = "hidden";
+        if( goBtn.hasClass("hidden") == false ){
+            goBtn.addClass("hidden");
         }
     }else{
-        if( nameInput.className == "error" ){
-            nameInput.className = "";
+        if(!nameValid){nameValid=true;}
+        if( nameInput.hasClass("error")){
+            nameInput.removeClass("error");
         }
-        if(goBtn.className == "hidden"){
-            goBtn.className = "";
+        if(goBtn.hasClass("hidden")){
+            goBtn.removeClass("hidden");
         }
     }
 }
-window.onload = function(){
-    init();
-};
+
+$(document).ready(init());
