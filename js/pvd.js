2ce3db6052f6363b602b51d94cc9c6b26ff16cb4
robotmayo <robotmayo@gmail.com>
2013-06-29 22:12:04 -0400

    Added hero block
    Code now generates hero block using the creatHeroBlock monster of a
    function.

--- js/pvd.js
+++ js/pvd.js
@@ -1,15 +1,18 @@
 // Page Elements
 var gameDiv;
 var enemyBlock;
-var enemyBlockList;
+var heroBlock;
 // Game
 var lastTime;
 var curTime;
 var elapsed; // Current frame time (delta) last frame time
 var chars; // List of all active characters
+var enemyList; // List of all enemys
+var enemyBlockList; // List of all enemy blocks
 
 // Player
 var player; // The player object
+var target; // The currently selected enemy
 // Enemy
 var enemy;
 
@@ -19,15 +22,26 @@ function startGame(playerName){
 	gameDiv = $("#game");
 	gameDiv.removeClass("hide");
 	enemyBlock = $("#enemies");
+	heroBlock = $("#heroes");
 	setUpGame(playerName);
 	gameLoop();
 }
 function setUpGame(playerName){
 	lastTime = Date.now();
 	curTime = 0;
-	player = new Hero(playerName);
+	player = new Knight(playerName);
+	heroBlock.append(createHeroBlock(player));
 	enemy = new Enemy(enemy_data.goblinKing);
+	enemyList = [];
+	enemyList.push(enemy);
 	var eb = createEnemyBlock(enemy);
+	eb[0].id = enemyList.length-1;
+	eb.on("click",function(){
+		$(this).closest("#enemies")
+        .find(".enemy").removeClass("selected-red");
+        $(this).addClass("selected-red");
+        target = enemyList[$(this)[0].id];
+	});
 	enemyBlockList = [];
 	enemyBlockList.push(eb);
 	enemyBlock.append(eb);
@@ -70,4 +84,36 @@ function createEnemyBlock(enemy){
 	enemyBlock.append(eBlock);
 	p = em = null;
 	return eBlock;
+}
+// Kill it with fire!
+function createHeroBlock(hero){
+	var container = $("<div></div>");
+	container.addClass("hero block-1-3");
+	container.append($("<img/>"));
+	var info = $("<div></div>");
+	info.addClass("info");
+	var temp = $("<h2></h2>");
+	temp.addClass("name");
+	temp.text(hero.name);
+	info.append(temp);
+	temp = $("<p></p>");
+	temp.addClass("desc");
+	var temp2 = $("<em></em>");
+	temp2.text(hero.desc);
+	temp.append(temp2);
+	info.append(temp);
+	container.append(info);
+	temp = $("<canvas></canvas>")
+	hero.setCanvas(temp[0]);
+	container.append(temp);
+	temp = $("<div></div>");
+	temp.addClass("inputs");
+	temp2 = $("<button></button>");
+	temp2[0].id = "attack";
+	temp2.text("Attack");
+	temp.append(temp2);
+	container.append(temp);
+	temp = info = temp2 = null;
+	container.find("#attack").on("click",function(){hero.useAttack(target)});
+	return container;
 }
