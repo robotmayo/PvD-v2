80cc8accd0cc75b1fee02fe9b166c754258a0ce6
robotmayo <robotmayo@gmail.com>
2013-06-29 22:09:07 -0400

    Added a data file.
    Contains enemy data and other stuff.

--- js/data.js
+++ js/data.js
@@ -7,5 +7,17 @@ var enemy_data = {
 		satb : 50,
 		matb : 100,
 		atbc : 35
+	},
+	bob : {
+		name : "Bob",
+		desc : "HERE'S BOBBY",
+		hp : 4000*3,
+		mp : 1000 / 4,
+		satb : 50,
+		matb : 100,
+		atbc : 35
 	}
+}
+var heroDesc = {
+	knight : "Brave and powerful, the knight will raise his blade against anyone who may threaten the peace of the world."
 }
