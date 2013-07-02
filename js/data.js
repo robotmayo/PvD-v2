239d3d5883dce053b7183ec1e7c4ee0d7aa4ce9f
robotmayo <robotmayo@gmail.com>
2013-07-01 22:03:47 -0400

    Added goblin enemy, hero data. Buffed goblin king

--- js/data.js
+++ js/data.js
@@ -1,23 +1,68 @@
-var enemy_data = {
+var enemyData = {
 	goblinKing : {
 		name : "Goblin King",
 		desc : "Despite what the name goblin implies, the Goblin King is a fearsome, hulking giant.",
-		hp : 4000,
-		mp : 1000,
-		satb : 50,
+		hp : 25000,
+		mp : 3000,
 		matb : 100,
-		atbc : 35
+		atbc : 10,
+		minDamage : 450,
+		maxDamage : 750,
+		critRate : 5,
+		critMul : 2.5
+	},
+	goblin : {
+		name : "Goblin",
+		desc : "Don't let their small stature fool, these creatures are every bit as deadly as ones 3x their size.",
+		hp : 7000,
+		mp : 80,
+		matb : 100,
+		atbc : 35,
+		minDamage : 150,
+		maxDamage : 350,
+		critRate : 10,
+		critMul : 1.2
 	},
 	bob : {
 		name : "Bob",
 		desc : "HERE'S BOBBY",
-		hp : 4000*3,
-		mp : 1000 / 4,
-		satb : 50,
+		hp : 99999,
+		mp : 999,
+		satb : 99,
 		matb : 100,
-		atbc : 35
+		atbc : 55,
+		minDamage : 500,
+		maxDamage : 999,
+		critRate : 40,
+		critMul : 1.4
 	}
-}
-var heroDesc = {
-	knight : "Brave and powerful, the knight will raise his blade against anyone who may threaten the peace of the world."
-}
+};
+var heroDesc = [
+	{
+		desc : "Brave and powerful, the knight will raise his blade against anyone who may threaten the peace of the world. Special : Valiant Charge, inflict extra damage based on your remaining health",
+		name : "Knight"
+	},
+	{
+		desc : "Something, something magic.",
+		name : "Wizard"
+	},
+	{
+		desc : "Something, something kickass robots",
+		name : "Machinist"
+	}
+];
+var heroData = {
+	"knight" : {
+		name : "Knight",
+		desc : "The valiant warrior of peace",
+		hp : 8000,
+		mp : 300,
+		satb : 0,
+		matb : 100,
+		atbc : 25,
+		minDamage : 500,
+		maxDamage : 900,
+		critRate : 5,
+		critMul : 1.5
+	}
+};
